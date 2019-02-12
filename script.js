2a0adaad4156da9c26369a3f69e8ecdf0891ae48
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-12 11:04:00 -0500

    Allow buying helpers

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 const types = {
-	stand: 1,
-	hydralicPress: 100,
+	stand: [30, 1],
+	hydralicPress: [250, 100],
+	factory: [120000, 100000],
 };
 const save = {
 	cash: 0,
 	multipliers: [],
-	helpers: Object.entries(types).map(d=>[d[0], 0]),
+	helpers: {},
 	...JSON.parse(localStorage.getItem("idler-save")),
 };
 
-let cps = 0;
-function calcCPS() {
-	const cps2 = 0;
-	Object.entries(save.helpers).map(([ key, val ]) => {
-		cps += types[key] * val;
-	});
-	return cps = cps2;
-}
-calcCPS();
-
-setInterval(() => {
-	save.cash += cps;
-	render();
-}, 750);
+Object.keys(types).forEach(type => {
+	if (!save.helpers[type]) {
+		save.helpers[type] = 0;
+	}
+});
 
 const appDiv = document.getElementById("app");
 const elem = React.createElement;
@@ -40,6 +32,22 @@ class Stat extends React.Component {
 		}, this.props.data);
 	}
 }
+
+let cps = 0;
+function calcCPS() {
+	let cps2 = 0;
+	Object.entries(save.helpers).map(([ key, val ]) => {
+		cps2 += types[key][1] * val;
+	});
+	render();
+	return cps = cps2;
+}
+calcCPS();
+
+setInterval(() => {
+	save.cash += cps;
+	render();
+}, 750);
 
 function render() {
 	localStorage.setItem("idler-save", JSON.stringify(save));
@@ -64,6 +72,29 @@ function render() {
 
 	children.push(elem("div", {
 		display: "flex",
+		style: {
+			padding: 10,
+			justifyContent: "space-evenly",
+		},
+		children: Object.entries(types).map(([type, [cost, cpsAdd]]) => {
+			return elem("button", {
+				style: {
+					margin: 6,
+					border: "none",
+					background: save.cash >= cost ? "#76ff03" : "red",
+					padding: 10,
+					borderRadius: 8,
+					flexGrow: 1,
+				},
+				onClick: () => {
+					if (save.cash >= cost) {
+						save.cash -= cost;
+						save.helpers[type] += 1;
+						calcCPS();
+					}
+				}
+			}, type + " (+" + cpsAdd + " cps)");
+		}),
 	}));
 
 	ReactDOM.render(elem("div", {
